867f580b2a90256f65438f15671de5d3c0fd619f
Bruno Carvalho <brunocarvalhodesa@gmail.com>
2026-07-10 20:59:43 -0300

    cypress login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Login", () => {
+
+  it("Should login successfully", () => {
+
+    const body = {
+      email: "brunocarvalhodesa@gmail.com",
+      password: "Teste@123"
+    };
+
+    cy.request({
+      method: "POST",
+      url: "/login",
+      body
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.message).to.eq('Login realizado com sucesso');
+    });
+
+  });
+
+});
